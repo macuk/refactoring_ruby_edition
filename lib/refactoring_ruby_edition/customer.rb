2da1c3806beac9389541d02ccb40116cf186bee6
Piotr Macuk <piotr@macuk.pl>
2015-05-18 23:45:49 +0200

    Code style improvements while rubocop session

--- lib/refactoring_ruby_edition/customer.rb
+++ lib/refactoring_ruby_edition/customer.rb
@@ -1,16 +1,16 @@
 module RefactoringRubyEdition
   class Customer
     attr_reader :name
-  
+
     def initialize(name)
       @name = name
       @rentals = []
     end
-  
+
     def add_rental(arg)
       @rentals << arg
     end
-  
+
     def statement
       result = "Rental Record for #{@name}\n"
       @rentals.each do |element|
@@ -19,7 +19,8 @@ module RefactoringRubyEdition
       end
       # add footer lines
       result += "Amount owed is #{total_charge}\n"
-      result += "You earned #{total_frequent_renter_points} frequent renter points"
+      result += "You earned #{total_frequent_renter_points} " \
+                'frequent renter points'
       result
     end
 
@@ -27,24 +28,25 @@ module RefactoringRubyEdition
       result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
       @rentals.each do |element|
         # show figures for this rental
-        result += "\t" + element.movie.title + ": " + element.charge.to_s + "<br>\n"
+        result += "\t" + element.movie.title + ': ' +
+                  element.charge.to_s + "<br>\n"
       end
       # add footer lines
       result += "<p>You owe <em>#{total_charge}</em><p>\n"
-      result += "On this rental you earned " +
-                "<em>#{total_frequent_renter_points}</em> " +
-                "frequent renter points<p>"
+      result += 'On this rental you earned ' \
+                "<em>#{total_frequent_renter_points}</em> " \
+                'frequent renter points<p>'
       result
     end
 
     private
 
     def total_charge
-      @rentals.inject(0) { |sum, rental| sum + rental.charge }
+      @rentals.inject(0) { |a, e| a + e.charge }
     end
 
     def total_frequent_renter_points
-      @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
+      @rentals.inject(0) { |a, e| a + e.frequent_renter_points }
     end
   end
 end
--- refactoring_ruby_edition.gemspec
+++ refactoring_ruby_edition.gemspec
@@ -9,16 +9,14 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Piotr Macuk"]
   spec.email         = ["piotr@macuk.pl"]
 
-  if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com' to prevent pushes to rubygems.org, or delete to allow pushes to any server."
-  end
-
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = 'Step by step refactoring with Refactoring book.'
+  spec.description   = ''
+  spec.homepage      = "https://github.com/macuk/refactoring_ruby_edition"
   spec.license       = "MIT"
 
-  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
+    f.match(%r{^(test|spec|features)/})
+  end
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
@@ -27,4 +25,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest"
   spec.add_development_dependency "simplecov"
+  spec.add_development_dependency "rubocop"
 end
