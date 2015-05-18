70d9701af5da2f6c951c4815b45f17a2e480420e
Piotr Macuk <piotr@macuk.pl>
2015-05-18 23:18:18 +0200

    Decomposing and Redistributing the Statement Method -- step 3
    Extracting Frequent Renter Points

--- lib/refactoring_ruby_edition/rental.rb
+++ lib/refactoring_ruby_edition/rental.rb
@@ -21,5 +21,9 @@ module RefactoringRubyEdition
       end
       result
     end
+
+    def frequent_renter_points
+      (movie.price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+    end
   end
 end
